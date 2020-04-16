9358318fadef5febd3766bc9ab4a1cead731528f
MM Mamunuzzaman <mamun@sgcsoft.net>
2020-04-16 15:14:27 +0600

    removed h1 from index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,6 @@ import Layout from "../components/layout"
 
 const IndexPage = ({ data }) => (
   <Layout>
-    <h1 className="sidebar__title">Recent posts from Call My Clinic Team</h1>
     <Masonry className="showcase">
       {data.allDatoCmsWork.edges.map(({ node: work }) => (
         <div key={work.id} className="showcase__item">
