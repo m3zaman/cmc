21e61a602d914de7598bf89b0f2698c9091c02f0
MM Mamunuzzaman <mamun@sgcsoft.net>
2020-04-16 15:09:32 +0600

    add H1 in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Layout from "../components/layout"
 
 const IndexPage = ({ data }) => (
   <Layout>
+    <h1 className="sheet__title">Recent posts from Call My Clinic Team</h1>
     <Masonry className="showcase">
       {data.allDatoCmsWork.edges.map(({ node: work }) => (
         <div key={work.id} className="showcase__item">
